34fc845f56a1b57286faf61a5503a384e42cabdf
Acer <Acer@DESKTOP-K09QF87>
2022-08-26 22:39:17 +0200

    Added src/sass folder for sass files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 
-const express = require('express');
+import express from 'express';
+import path from 'path';
 const app = express();
+const PORT: number = 3000;
+const public_path: string = path.join('dist','public');
 
-app.use(express.static('public'));
+app.use(express.static(public_path));
 
 app.get('/',(req,res)=>{
     res.send('Accedi al tuo account');
 });
 
-app.listen(3000);
+app.listen(PORT,()=>{
+    console.log(`Server in esecuzione sulla porta ${PORT}`);
+});
