b04ff278255a0aed18049ba05013de97ae4c41aa
Acer <Acer@DESKTOP-K09QF87>
2022-08-26 22:48:05 +0200

    Added src/modules/static_paths.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 
 import express from 'express';
+import { StaticPaths } from './modules/static_paths';
 import path from 'path';
 const app = express();
 const PORT: number = 3000;
 const public_path: string = path.join('dist','public');
 
-app.use(express.static(public_path));
+
+app.use(express.static(StaticPaths.public_path));
+app.use('/css',express.static(StaticPaths.public_path_css));
 
 app.get('/',(req,res)=>{
     res.send('Accedi al tuo account');
---
+++ src/modules/static_paths.ts
@@ -0,0 +1,9 @@
+
+import path from "path";
+
+export namespace StaticPaths{
+    export const public_path: string = path.join('dist','public');
+    export const public_path_css: string = path.join('dist','public','css');
+}
+
+
