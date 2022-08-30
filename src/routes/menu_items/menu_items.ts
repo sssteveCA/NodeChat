5ddbf38166589171d9084f2090195015b24185a5
Acer <Acer@DESKTOP-K09QF87>
2022-08-30 20:21:31 +0200

    Added rules.mustache & terms.mustache in dist/views/partials

--- src/routes/menu_items/menu_items.ts
+++ src/routes/menu_items/menu_items.ts
@@ -1,21 +1,31 @@
 //Menu items routes
 
 import express from 'express';
+import { Constants } from '../../constants';
 
 export const menu_items_routes = express.Router();
 
 menu_items_routes.get('/about_us',(req,res)=>{
+    res.render('about_us',{
+        container: Constants.CONTAINER
+    })
 });
 
 menu_items_routes.get('/contacts',(req,res)=>{
-
+    res.render('contacts',{
+        container: Constants.CONTAINER
+    });
 });
 
 menu_items_routes.get('/rules',(req,res)=>{
-
+    res.render('rules',{
+        container: Constants.CONTAINER
+    });
 });
 
 menu_items_routes.get('/terms',(req,res)=>{
-
+    res.render('terms',{
+        container: Constants.CONTAINER
+    });
 });
 
