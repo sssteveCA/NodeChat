43ac423e20aba81952e544f7bb0295d94d054ae2
Acer <Acer@DESKTOP-K09QF87>
2022-09-05 17:54:57 +0200

    Added assignValues method in emailrequest.ts

--- src/public/js/classes/request/emailrequest.ts
+++ src/public/js/classes/request/emailrequest.ts
@@ -11,13 +11,35 @@ export class EmailRequest{
     private _email: string;
     private _subject: string;
     private _message: string;
+    private _errno: number = 0;
+    private _error: string|null = null;
 
     constructor(data: EmailRequestInterface){
-
+        this.assignValues(data);
     }
 
     get name(){return this._name;}
     get email(){return this._email;}
     get subject(){return this._subject;}
     get message(){return this._message;}
+    get errno(){return this._errno;}
+    get error(){
+        switch(this._errno){
+            default:
+                this._error = null;
+                break;
+        }
+        return this._error;
+    }
+
+    private assignValues(data: EmailRequestInterface){
+        if(data.name)this._name = data.name;
+        else this._name = "";
+        if(data.email)this._email = data.email;
+        else this._email = "";
+        if(data.subject)this._subject = data.subject;
+        else this._subject = "";
+        if(data.message)this._message = data.message;
+        else this._message = "";
+    }
 }
