19b3bb67fc8035f8a9e6e89d29333282e7fa7cab
Acer <Acer@DESKTOP-K09QF87>
2022-09-05 13:32:04 +0200

    Added contact_validator middleware in email.ts

--- src/routes/email/email.ts
+++ src/routes/email/email.ts
@@ -1,9 +1,39 @@
 
 //Email routes
-import express from 'express';
+import bodyParser from 'body-parser';
+import express, { NextFunction, Request, Response } from 'express';
 
 const email_routes = express.Router();
 
 email_routes.post('/send_email',(req,res)=>{
-    console.log(req.body);
+    
 });
+
+//Validate email contact values
+const contact_validator = (req: Request, res: Response ,next: NextFunction) => {
+    console.log(req.body);
+    let body: object = req.body as object;
+    if(body != null){
+        if(body.hasOwnProperty('name') && body.hasOwnProperty('email') && body.hasOwnProperty('subject') && body.hasOwnProperty('message')){
+            let valid: boolean = true;
+            for(const [key,value] of Object.entries(body)){
+                if(value == ''){
+                    valid = false;
+                    break;
+                }
+                if(key == 'email'){
+                    let regex: RegExp = new RegExp('^[a-zA-Z]{4,30}@([a-zA-Z0-9]{3,30}\.){1,5}[a-z]{2,10}$');
+                    if(!regex.test(value)){
+                        valid = false;
+                        break;
+                    }
+                }//if(key == 'email'){
+            }//for(const [key,value] of Object.entries(body)){
+            if(valid == true)
+               next();
+            else
+                res.status(400).send({done: false, msg: "I dati inseriti non sono nel formato corretto, riprova"});
+        }//if(body.hasOwnProperty('name') && body.hasOwnProperty('email') && body.hasOwnProperty('subject') && body.hasOwnProperty('message')){ 
+    }//if(body != null){
+    res.status(400).send({done: false, msg: "Inserisci i dati richiesti per continuare"});
+};
