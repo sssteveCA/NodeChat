969547c766e421580ab8fcf06b43272e2e03a976
Acer <Acer@DESKTOP-K09QF87>
2022-09-05 16:43:28 +0200

    Added jquery_js route in scripts.ts

--- src/routes/js/scripts.ts
+++ src/routes/js/scripts.ts
@@ -6,7 +6,12 @@ import { Paths } from '../../paths';
 
 export const scripts_router = express.Router();
 
+scripts_router.get('/jquery_js',(req,res)=>{
+    const jquery_js: string = path.join('node_modules','jquery','dist','jquery.min.js');
+    res.sendFile(jquery_js,{root: Paths.ROOTPATH+'../'});
+});
+
 scripts_router.get('/jquery_ui_js',(req,res)=>{
     const jquery_ui_js: string = path.join('node_modules','jquery-ui-dist','jquery-ui.min.js');
-    res.sendFile(jquery_ui_js,{root: Paths.ROOTPATH+'/../'})
+    res.sendFile(jquery_ui_js,{root: Paths.ROOTPATH+'/../'});
 });
