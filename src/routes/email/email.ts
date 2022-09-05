4e3e843fdd4ba634b73a1d2a5c420f4f620a37c8
Acer <Acer@DESKTOP-K09QF87>
2022-09-05 15:14:23 +0200

    Added response for '/send_email' route in email.ts

--- src/routes/email/email.ts
+++ src/routes/email/email.ts
@@ -2,6 +2,7 @@
 //Email routes
 import bodyParser from 'body-parser';
 import express, { NextFunction, Request, Response } from 'express';
+import { EmailInterface, Email } from '../../classes/email/email';
 
 export const email_routes = express.Router();
 
@@ -35,6 +36,18 @@ const contact_validator = (req: Request, res: Response ,next: NextFunction) => {
 };
 
 email_routes.post('/send_email',contact_validator,async (req,res)=>{
-    
+    let em_data: EmailInterface = {
+        name: req.body.name,
+        email: req.body.email,
+        subject: req.body.subject,
+        message: req.body.message
+    };
+    let em: Email = new Email(em_data);
+    em.sendMail().then(obj => {
+        if(obj['done'] == true)
+            res.status(200).send(obj);
+        else
+            res.status(500).send(obj);
+    });
 });
 
