640f4503104f8aec2d7b91df3bf65067bd7fe217
Acer <Acer@DESKTOP-K09QF87>
2022-09-05 15:03:38 +0200

    Added sendMail method in email.ts class

--- src/classes/email/email.ts
+++ src/classes/email/email.ts
@@ -21,6 +21,12 @@ export class Email{
     private _transport: nodemailer.Transporter;
     private _message: string;
     private _message_object: MessageInterface;
+    private _errno: number = 0;
+    private _error: string|null = null;
+
+    public static ERR_SEND:number = 1;
+
+    private static ERR_SEND_MSG:string = "Errore durante l'invio della mail";
 
     constructor(data: EmailInterface){
         this.setTransport();
@@ -31,6 +37,18 @@ export class Email{
     get email(){return this._email;}
     get subject(){return this._subject;}
     get message(){return this._message;}
+    get errno(){return this._errno;}
+    get error(){
+        switch(this._errno){
+            case Email.ERR_SEND:
+                this._error = Email.ERR_SEND_MSG;
+                break;
+            default:
+                this._error = null;
+                break;
+        }
+        return this._error;
+    }
 
     /**
      * Set the nodemailer transporter
@@ -58,6 +76,27 @@ export class Email{
         };
     }
 
-    
+    public async sendMail(): Promise<object>{
+        let response: object = {};
+        this._errno = 0;
+        try{
+            await this._transport.sendMail(this._message_object).then(res => {
+                response = {
+                    done: true,
+                    msg: "La tua richiesta è stata inviata. Riceverai una risposta nel minor tempo possibile"
+                };
+            }).catch(err => {
+                console.warn(err);
+                throw err;
+            });
+        }catch(e){
+            this._errno = Email.ERR_SEND;
+            response = {
+                done: false,
+                msg: this.error
+            };
+        }
+        return response;
+    }
     
 }
