d55d490e8b2fb94d15d2c88fb4ebc2bd8db2feda
Acer <Acer@DESKTOP-K09QF87>
2022-09-13 18:26:27 +0200

    Added src/classes/authentication/subscribe.ts

--- src/classes/authentication/subscribe.ts
+++ src/classes/authentication/subscribe.ts
@@ -1,31 +1,24 @@
-import { ValidationError } from "../../errors/validationerror.js";
 
-export interface SubscribeRequestInterface{
-    username:string;
-    email:string;
-    password:string;
-    confPass:string;
+export interface SubscribeInterface{
+    username: string;
+    email: string;
+    password: string;
 }
 
-export class SubscribeRequest{
+export class Subscribe{
     private _username:string;
     private _email:string;
     private _password:string;
-    private _confPass:string;
     private _errno:number = 0;
     private _error:string|null = null;
 
-    constructor(data: SubscribeRequestInterface){
-        this._username = data.username;
-        this._email = data.email;
-        this._password = data.password;
-        this._confPass = data.confPass;
+    constructor(data: SubscribeInterface){
+        
     }
 
     get username(){return this._username; }
     get email(){return this._email; }
     get password(){return this._password; }
-    get confPass(){return this._confPass; }
     get errno(){return this._errno; }
     get error(){
         switch(this._errno){
