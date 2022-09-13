bd20f8fc5a2bfee2fa1e8df3338fdc91db3ba584
Acer <Acer@DESKTOP-K09QF87>
2022-09-13 13:22:51 +0200

    Added bootstrap.min.css route in styles.ts

--- src/paths.ts
+++ src/paths.ts
@@ -1,9 +1,12 @@
 
 export namespace Paths{
     export const ROOTPATH = __dirname;
-    export const JQUERY_JS = "js2/jquery_js";
-    export const JQUERY_UI_CSS = "css2/jquery_ui_css";
-    export const JQUERY_UI_JS = "js2/jquery_ui_js";
+    export const CSS2 = "css2/";
+    export const JS2 = "js2/";
+    export const BOOTSTRAP_CSS = CSS2+"/bootstrap_css";
+    export const JQUERY_JS = JS2+"jquery_js";
+    export const JQUERY_UI_CSS = CSS2+"jquery_ui_css";
+    export const JQUERY_UI_JS = JS2+"jquery_ui_js";
     export const LOGIN = "/login";
     export const NEWACCOUNT = "/newAccount";
     export const SEND_EMAIL = "/send_email";
