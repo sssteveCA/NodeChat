f737aeb5cf9b90d75b6e681f313c765243a806d4
Acer <Acer@DESKTOP-K09QF87>
2022-09-16 19:08:18 +0200

    Added setHtmlMail method in emailverify.ts

--- src/classes/email/emailverify.ts
+++ src/classes/email/emailverify.ts
@@ -1,21 +1,40 @@
 
 export interface EmailVerifyInterface{
-    home_url: string;
-    route_name: string;
+    email_verify_url: string;
     activation_code: string;
 }
 
 export class EmailVerify{
-    private _home_url: string;
-    private _route_name: string;
     private _activation_code: string;
     private _mail_body: string = "";
+    private _email_verify_url: string;
+    private _full_activation_link: string;
 
     constructor(data: EmailVerifyInterface){
-        this._home_url = data.home_url;
-        this._route_name = data.route_name;
         this._activation_code = data.activation_code;
+        this._email_verify_url = data.email_verify_url;
+        this._full_activation_link = `${this._email_verify_url}/${this._activation_code}`;
     }
 
     get activation_code(){return this._activation_code;}
+    get mail_body(){return this._mail_body;}
+    get full_activation_link(){return this._full_activation_link;}
+
+    private setHtmlMail(): void{
+        this._mail_body = `
+<div>
+    <div class="thank-you">
+        Grazie per esserti iscritto al nostro servizio di chat. <br>
+        Per completare l'attivazione clicca su <a href="${this._full_activation_link}">questo link</a>
+    </div>
+    <div class="otherwise">
+        In alternativa puoi incollare questo codice: ${this._activation_code} <br>,
+        a <a href="${this._email_verify_url}">questo link</a>
+    </div>
+    <div class="staff">
+        Lo staff di NodeChat
+    </div>
+</div>
+        `;
+    }
 }
