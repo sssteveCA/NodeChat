0a6c6dca6fab6776e01f49a0d506f156745a4d71
Acer <Acer@DESKTOP-K09QF87>
2022-09-19 23:27:27 +0200

    Added confirm dialog on logout link click in menu.ts

--- src/public/js/menu.ts
+++ src/public/js/menu.ts
@@ -1,10 +1,26 @@
+import { ConfirmDialog, ConfirmDialogInterface } from "./classes/dialogs/confirmdialog";
 
 $(()=>{
+    console.log("menu.js");
     let logout_link:JQuery = $('#logout');
     if(logout_link.length){
         //If logout_link exists
         logout_link.on("click",(e)=>{
             e.preventDefault();
+            let cd_data: ConfirmDialogInterface = {
+                title: "Esci dalla sessione",
+                message: "Sei sicuro di voler chiudere la sessione?"
+            };
+            let cd: ConfirmDialog = new ConfirmDialog(cd_data);
+            cd.btYes.on('click',()=>{
+                cd.dialog.dialog('destroy');
+                cd.dialog.remove();
+                window.location.href = "/logout";
+            });
+            cd.btNo.on('click', ()=>{
+                cd.dialog.dialog('destroy');
+                cd.dialog.remove();
+            });
         });
     }//if(logout_link.length){
 });
