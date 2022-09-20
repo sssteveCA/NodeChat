08bbdea8d6828fc4947d405176b49cbab9a5d229
Acer <Acer@DESKTOP-K09QF87>
2022-09-20 09:20:41 +0200

    Added token validation instructions to logged middleware in middlewares.ts

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -1,5 +1,9 @@
 //Middlewares used in different routes
 import express, { NextFunction, Request, Response } from 'express';
+import { MongoDbModelManagerInterface } from '../classes/database/mongodbmodelmanager';
+import { Schemas } from '../namespaces/schemas';
+import {TokenInterface, Token} from '../classes/database/models/token'
+import { Messages } from '../namespaces/messages';
 
 /**
  * Pass to the next hop if user is not logged 
@@ -13,9 +17,37 @@ import express, { NextFunction, Request, Response } from 'express';
 /**
  * Pass to the next hop if user is logged
  */
-export const logged = (req:Request, res: Response, next: NextFunction) => {
+export const logged = async(req:Request, res: Response, next: NextFunction) => {
     if(req.session['username'] && req.session['token_key']){
-        return next();
+        let username: string = req.session['username'];
+        let token_key: string = req.session['token_key'];
+        let mongo_mmi: MongoDbModelManagerInterface = {
+            collection_name: process.env.MONGODB_TOKENS_COLLECTION as string,
+            schema: Schemas.TOKENS
+        };
+        let token_data: TokenInterface = {};
+        let token: Token = new Token(mongo_mmi,token_data);
+        await token.getToken({tokenKey: token_key}).then(result => {
+            if(result['errno'] == 0 && result['result'] != null){
+                let nowTimestamp: number = Date.now();
+                let expDate: Date = new Date(result['result']['expireDate']);
+                let expTime: number = expDate.getTime();
+                console.log("nowTimestamp => "+nowTimestamp);
+                console.log("expTime => "+expTime);
+                if(nowTimestamp < expTime) return next();
+                else{
+                    req.session['username'] = null;
+                    req.session['token_key'] = null;
+                    req.session.destroy(()=>{
+                        let message_encoded = encodeURIComponent(Messages.ERROR_SESSIONEXPIRED);
+                        return res.redirect("/login?message="+message_encoded);
+                    });
+                }
+            }//if(result['errno'] == 0 && result['result'] != null){
+            else return res.redirect("/login");
+        }).catch(err => {
+            return res.redirect("/login");
+        });
     }
     else return res.redirect("/login");
     
--- src/namespaces/messages.ts
+++ src/namespaces/messages.ts
@@ -3,5 +3,6 @@ export namespace Messages{
     export const ERROR_INCORRECTFORMAT: string = "I dati inseriti non sono nel formato corretto, riprova";
     export const ERROR_INVALIDCREDENTIALS: string = "Il nome utente o la password non sono corretti, riprova";
     export const ERROR_MISSINGDATA: string = "Inserisci i dati richiesti per continuare";
+    export const ERROR_SESSIONEXPIRED: string = "La tua sessione è scaduta";
 
 }
