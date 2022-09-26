876a2af4e3f8207bae36e8085d17f377397b8b30
Acer <Acer@DESKTOP-K09QF87>
2022-09-26 20:01:27 +0200

    Fixed body::before background-image height to full viewport with scroll

--- src/namespaces/paths.ts
+++ src/namespaces/paths.ts
@@ -1,13 +1,13 @@
 
 export namespace Paths{
     export const ROOTPATH = __dirname+'/../';
-    export const CSS2 = "css2/";
-    export const JS2 = "js2/";
+    export const CSS2 = "css2";
+    export const JS2 = "js2";
     export const BOOTSTRAP_CSS = CSS2+"/bootstrap_css";
     export const BOOTSTRAP_JS = JS2+"/bootstrap_js";
-    export const JQUERY_JS = JS2+"jquery_js";
-    export const JQUERY_UI_CSS = CSS2+"jquery_ui_css";
-    export const JQUERY_UI_JS = JS2+"jquery_ui_js";
+    export const JQUERY_JS = JS2+"/jquery_js";
+    export const JQUERY_UI_CSS = CSS2+"/jquery_ui_css";
+    export const JQUERY_UI_JS = JS2+"/jquery_ui_js";
     export const LOGIN = "/login";
     export const NEWACCOUNT = "/newAccount";
     export const SEND_EMAIL = "/send_email";
--- src/public/js/footer.ts
+++ src/public/js/footer.ts
@@ -10,6 +10,7 @@ $(()=>{
  * Change footer position, comparing the window & container heights
  */
 function setFooter(): void{
+    let body: JQuery = $('body');
     let container: JQuery = $('.container');
     let footer: JQuery = $('.footer');
     let menu: JQuery = $('.menu-bar');
@@ -21,8 +22,10 @@ function setFooter(): void{
     }
     if(heights['cHeight'] < (heights['wHeight'] - heights['fHeight'] - heights['mHeight'] - 50)){
         footer.css({position: 'fixed',bottom: '0'});
+        body.css('position','');
     }// if(heights['cHeight'] < (heights['wHeight'] - heights['fHeight'] - heights['mHeight'] - 50)){
     else{
+        body.css('position','relative');
         footer.css({position: '',bottom: ''});
     }
 }
