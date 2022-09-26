8339802caaec4d3d35d02f4f6f48b78ba49400ed
Acer <Acer@DESKTOP-K09QF87>
2022-09-26 17:50:51 +0200

    Added src/public/css/footer.sass

--- src/public/js/footer.ts
+++ src/public/js/footer.ts
@@ -1,7 +1,13 @@
 
 $(()=>{
+    let container: JQuery = $('.container');
+    let footer: JQuery = $('.footer');
     let wWidth: number = $(window).height() as number;
-    let cWidth: number = $('.container').height() as number;
-    console.log("wWidth => "+wWidth);
-    console.log("cWidth => "+cWidth);
-})
+    let cWidth: number = container.height() as number;
+    if(cWidth < wWidth){
+        footer.css({
+            position: 'fixed',
+            bottom: '0',
+        });
+    }//if(cWidth < wWidth){
+});
