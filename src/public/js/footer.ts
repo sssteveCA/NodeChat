896c97db5f8327da6cbda357476b9bda71513aa3
Acer <Acer@DESKTOP-K09QF87>
2022-09-26 18:51:32 +0200

    Added setFooter function in footer.ts

--- src/public/js/footer.ts
+++ src/public/js/footer.ts
@@ -1,13 +1,28 @@
 
 $(()=>{
+    setFooter();
+    $(window).on('resize',()=>{
+        setFooter();
+    });
+});
+
+/**
+ * Change footer position, comparing the window & container heights
+ */
+function setFooter(): void{
     let container: JQuery = $('.container');
     let footer: JQuery = $('.footer');
-    let wWidth: number = $(window).height() as number;
-    let cWidth: number = container.height() as number;
-    if(cWidth < wWidth){
-        footer.css({
-            position: 'fixed',
-            bottom: '0',
-        });
-    }//if(cWidth < wWidth){
-});
+    let menu: JQuery = $('.menu-bar');
+    let heights: object = {
+        wHeight: $(window).height() as number,
+        cHeight: container.height() as number,
+        fHeight: footer.height() as number,
+        mHeight: menu.height() as number
+    }
+    if(heights['cHeight'] < (heights['wHeight'] - heights['fHeight'] - heights['mHeight'] - 50)){
+        footer.css({position: 'fixed',bottom: '0'});
+    }// if(heights['cHeight'] < (heights['wHeight'] - heights['fHeight'] - heights['mHeight'] - 50)){
+    else{
+        footer.css({position: '',bottom: ''});
+    }
+}
