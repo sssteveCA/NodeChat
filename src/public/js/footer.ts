ad45311eb669b97cc074a7415b5b84d15a04121e
Acer <Acer@DESKTOP-K09QF87>
2022-09-26 17:28:25 +0200

    Added src/public/js/footer.ts

---
+++ src/public/js/footer.ts
@@ -0,0 +1,7 @@
+
+$(()=>{
+    let wWidth: number = $(window).height() as number;
+    let cWidth: number = $('.container').height() as number;
+    console.log("wWidth => "+wWidth);
+    console.log("cWidth => "+cWidth);
+})
