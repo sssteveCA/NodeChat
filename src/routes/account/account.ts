89195454450e9facdebf963c2dc5820d43b73118
Acer <Acer@DESKTOP-K09QF87>
2022-09-28 20:37:16 +0200

    Fixed 'profile/:username' route scripts load in routes account.ts

--- src/routes/account/account.ts
+++ src/routes/account/account.ts
@@ -7,10 +7,14 @@ import { Paths } from '../../namespaces/paths';
 
 export const account_routes = express.Router();
 
-account_routes.get("/profile", logged, (req,res)=>{
-    return res.render('logged/profile', {
-        bootstrap_css: Paths.BOOTSTRAP_CSS, bootstrap_js: Paths.BOOTSTRAP_JS,
-        container: Constants.CONTAINER, jquery_js: Paths.JQUERY_JS, jquery_ui_css: Paths.JQUERY_UI_CSS, 
-        jquery_ui_js: Paths.JQUERY_UI_JS
-    });
+account_routes.get("/profile/:username", logged, (req,res)=>{
+    let username: string = req.params.username;
+    let current_username: string = req.session['username'];
+    if(username == current_username){
+        return res.render('logged/profile', {
+            bootstrap_css: "../"+Paths.BOOTSTRAP_CSS, bootstrap_js: "../"+Paths.BOOTSTRAP_JS,
+            container: Constants.CONTAINER, jquery_js: "../"+Paths.JQUERY_JS, jquery_ui_css: "../"+Paths.JQUERY_UI_CSS, 
+            jquery_ui_js: "../"+Paths.JQUERY_UI_JS, username: username
+        });
+    }//if(username == current_username){
 });
