36bad4c0809127efa7d884228a3606510a507093
Acer <Acer@DESKTOP-K09QF87>
2022-09-30 14:57:21 +0200

    Added loggedApi middleware in api middlewares.ts

--- src/namespaces/messages.ts
+++ src/namespaces/messages.ts
@@ -1,9 +1,11 @@
 
 export namespace Messages{
+    const ADMIN_CONTACT: string = "Se il problema persiste, contatta l'amministratore del sito";
     export const ERROR_INCORRECTFORMAT: string = "I dati inseriti non sono nel formato corretto, riprova";
     export const ERROR_INVALIDCREDENTIALS: string = "Il nome utente o la password non sono corretti, riprova";
     export const ERROR_MISSINGDATA: string = "Inserisci i dati richiesti per continuare";
     export const ERROR_NOTAUTHENTICATED: string = "Non sei autenticato";
+    export const ERROR_SERVER: string = "Siamo spiacenti, il server non è in grado di elaborare la tua richiesta. "+ADMIN_CONTACT;
     export const ERROR_SESSIONEXPIRED: string = "La tua sessione è scaduta";
 
 }
--- src/routes/api/middlewares/middlewares.ts
+++ src/routes/api/middlewares/middlewares.ts
@@ -1,6 +1,10 @@
 
 import express, { NextFunction, Request, Response } from 'express';
+import { Token, TokenInterface } from '../../../classes/database/models/token';
+import { MongoDbModelManagerInterface } from '../../../classes/database/mongodbmodelmanager';
+import { NotAuthenticatedError } from '../../../classes/errors/notauthenticatederror';
 import { Messages } from '../../../namespaces/messages';
+import { Schemas } from '../../../namespaces/schemas';
 
 /**
  * /**
@@ -8,11 +12,27 @@ import { Messages } from '../../../namespaces/messages';
  */
 export const loggedApi = async(req: Request, res: Response, next: NextFunction) => {
     let token_key: string|null = req.body['token_key'] ? req.body['token_key'] : null;
-    if(token_key){
-        
-    }//if(token_key){
-    return res.status(401).json({
-        done: false,
-        msg: Messages.ERROR_NOTAUTHENTICATED
-    });
+    try{
+        if(token_key){
+            let mmmi_data: MongoDbModelManagerInterface = {
+                collection_name: process.env.MONGODB_TOKENS_COLLECTION as string, schema: Schemas.TOKENS
+            };
+            let token_data: TokenInterface = {};
+            let token: Token = new Token(mmmi_data,token_data);
+            await token.getToken({tokenKey: token_key}).then(obj => {
+                if(obj['done'] == true && obj['result'] != null) return next();
+                else throw new NotAuthenticatedError("");
+            }).catch(err => {
+                throw err;
+            });
+        }//if(token_key){
+        else throw new NotAuthenticatedError("");
+    }catch(e){
+        if(e instanceof NotAuthenticatedError){
+            return res.status(401).json({done: false, msg: Messages.ERROR_NOTAUTHENTICATED});
+        }
+        else{
+            return res.status(500).json({done: false, msg: Messages.ERROR_SERVER});
+        }
+    }
 };
