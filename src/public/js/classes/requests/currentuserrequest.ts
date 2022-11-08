aafd241ec71f65f1a4f3d24b1febe1f90d90bfe4
Acer <Acer@DESKTOP-K09QF87>
2022-11-08 21:49:10 +0100

    Added properties, constructor and getters to currentuserrequest.ts

--- src/public/js/classes/requests/currentuserrequest.ts
+++ src/public/js/classes/requests/currentuserrequest.ts
@@ -1,8 +1,33 @@
 
 export interface CurrentUserRequestInterface{
-
+    token_key: string;
 }
 
 export class CurrentUserRequest{
-    
+    private _token_key: string;
+    private _errno: number = 0;
+    private _error: string|null = null;
+
+    public static ERR_FETCH:number = 1;
+
+    private static ERR_FETCH_MSG:string = "Errore durante l'esecuzione della richiesta";
+
+    constructor(data: CurrentUserRequestInterface){
+        this._token_key = data.token_key;
+    }
+
+    get errno(){return this._errno;}
+    get error(){
+        switch(this._errno){
+            case CurrentUserRequest.ERR_FETCH:
+                this._error = CurrentUserRequest.ERR_FETCH_MSG;
+                break;
+            default:
+                this._error = null;
+                break;
+        }
+        return this._error;
+    }
+
+
 }
