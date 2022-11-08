3a4c5733954ce15f0bf5040e2e82b5de47501efd
Acer <Acer@DESKTOP-K09QF87>
2022-11-08 22:05:29 +0100

    Added currentUser and currentUserPromise methods to currentuserrequest.ts

--- src/public/js/classes/requests/currentuserrequest.ts
+++ src/public/js/classes/requests/currentuserrequest.ts
@@ -12,6 +12,8 @@ export class CurrentUserRequest{
 
     private static ERR_FETCH_MSG:string = "Errore durante l'esecuzione della richiesta";
 
+    private static FETCH_URL:string = "/api/current_user";
+
     constructor(data: CurrentUserRequestInterface){
         this._token_key = data.token_key;
     }
@@ -27,6 +29,41 @@ export class CurrentUserRequest{
                 break;
         }
         return this._error;
+    }
+
+    public async currentUser(): Promise<object>{
+        this._errno = 0;
+        let response: object = {};
+        try{
+            await this.currentUserPromise().then(res => {
+                console.log(res);
+                response = JSON.parse(res);
+            }).catch(err => {
+                throw err;
+            });
+        }catch(e){
+            this._errno = CurrentUserRequest.ERR_FETCH;
+            response = { done: false, msg: this.error }
+        }
+        return response;
+    }
+
+    private async currentUserPromise(): Promise<string>{
+        return await new Promise<string>((resolve, reject) => {
+            let post_data: URLSearchParams = new URLSearchParams();
+            post_data.append("token_key",this._token_key);
+            fetch(CurrentUserRequest.FETCH_URL,{
+                method: "POST",
+                headers: {
+                    "Accept": "application/json", "Content-Type": "application/json"
+                },
+                body: post_data
+            }).then(res => {
+                resolve(res.text());
+            }).catch(err => {
+                reject(err);
+            });
+        });
     }
 
 
