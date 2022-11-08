f2acb4c2ed0c1e915f2238c5b5d5a0fa56b061bd
Acer <Acer@DESKTOP-K09QF87>
2022-11-08 18:57:23 +0100

    Added properties, constructor and getters to src/public/js/classes/requests/myprofilesectionrequests.ts

--- src/public/js/classes/requests/myprofilesectionsrequest.ts
+++ src/public/js/classes/requests/myprofilesectionsrequest.ts
@@ -1,4 +1,33 @@
+import { MyProfileSections } from "../../enums/enums";
 
-export interface MyProfileSectionsRequestsInterfaces {
-    
+export interface MyProfileSectionsRequestsInterface {
+    section: MyProfileSections
+}
+
+export class MyProfileSectionsRequest{
+    private _section: MyProfileSections;
+    private _errno: number = 0;
+    private _error: string|null = null;
+
+    constructor(data: MyProfileSectionsRequestsInterface){
+        this._section = data.section;
+    }
+
+    public static ERR_FETCH: number = 1;
+
+    public static ERR_FETCH_MSG: string = "Si è verificato un' errore durante l'esecuzione della richiesta";
+
+    get errno(){return this._errno;}
+    get error(){
+        switch(this._errno){
+            case MyProfileSectionsRequest.ERR_FETCH:
+                this._error = MyProfileSectionsRequest.ERR_FETCH_MSG;
+                break;
+            default:
+                this._error = null;
+                break;
+        }
+        return this._error;
+    }
+
 }
