b6eeabc70b594b3a94471e574aa2ca7d0c77c433
Acer <Acer@DESKTOP-K09QF87>
2022-11-08 22:55:35 +0100

    Added enum SectionLiItems to profile.ts

--- src/public/js/logged/profile.ts
+++ src/public/js/logged/profile.ts
@@ -8,5 +8,16 @@ $(()=>{
     let cur: CurrentUserRequest = new CurrentUserRequest(cur_data);
     cur.currentUser().then(obj => {
         console.log(obj);
+        $('#li-information').trigger('click');
+    });
+    $('.sections ul li').on('click',(e)=>{
+        let fired: JQuery = $(e.target);
+        console.log("sections fired");
+        let idTarget: string = fired.attr("id") as string;
+        console.log(idTarget);
     });
 });
+
+enum SectionLiItems{
+    ACCOUNT_SETTINGS = "li-account-settings", FRIENDS = "li-friends", INFORMATION = "li-information", PHOTOS = "li-photos", VIDEOS = "li-videos"
+}
