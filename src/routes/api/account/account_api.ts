33d8331a0781ba43d98c24c153a2373e0b2ea3f8
Acer <Acer@DESKTOP-K09QF87>
2022-11-08 19:59:18 +0100

    Added get account by token key operations to '/current_user' route in account_api.ts

--- src/routes/api/account/account_api.ts
+++ src/routes/api/account/account_api.ts
@@ -1,7 +1,10 @@
 
 import express from 'express';
+import { ObjectId } from 'mongoose';
+import { Account } from '../../../classes/database/models/account';
 import { Accounts, AccountsInterface } from '../../../classes/database/models/accounts';
 import { Token } from '../../../classes/database/models/token';
+import { MongoDbModelManagerInterface } from '../../../classes/database/mongodbmodelmanager';
 import { MongoDbModelsManagerInterface } from '../../../classes/database/mongodbmodelsmanager';
 import { MyProfileSections } from '../../../enums/enums';
 import { Messages } from '../../../namespaces/messages';
@@ -14,18 +17,35 @@ account_routes_api.post('/profile/section/:section', loggedApi, async(req,res)=>
     let tokenKey: string = res.locals["tokenKey"];
     let section: string = req.params['section'];
     if((<any>Object).values(MyProfileSections).includes(section)){
-        let mmis_data: MongoDbModelsManagerInterface = {
+        let mmi_data: MongoDbModelManagerInterface = {
             collection_name: process.env.MONGODB_TOKENS_COLLECTION as string,
             schema: Schemas.TOKENS
         };
-        let token: Token = new Token(mmis_data,{});
+        let token: Token = new Token(mmi_data,{});
         await token.getToken({tokenKey: tokenKey}).then(obj => {
-            res.status(200).json(obj);
-        }).catch(err => {
-
+            if(obj["done"] == true){
+                let accountId = obj["result"]["accountId"];
+                return accountId;
+            }
+            else throw new Error("");
+        }).then(accountId => {
+            let mmi_data: MongoDbModelManagerInterface = {
+                collection_name: process.env.MONGODB_ACCOUNTS_COLLECTION as string, schema: Schemas.ACCOUNTS
+            };
+            let account: Account = new Account(mmi_data,{});
+            return account.getAccount({_id: accountId});
+        }).then(obj => {
+            console.log(obj);
+            return res.status(200).json(obj);
         })
+        .catch(err => {
+            console.warn(err);
+            return res.status(500).json({
+                done: false, msg: Messages.ERROR_SERVER
+            });
+        });
     }//if((<any>Object).values(MyProfileSections).includes(section)){
-    return res.status(404).json({msg: "Sezione profilo non trovata"});
+    else return res.status(404).json({msg: "Sezione profilo non trovata"});
 })
 
 account_routes_api.post('/profile/search', loggedApi, async(req,res)=>{
