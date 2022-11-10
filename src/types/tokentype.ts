612db5a871845a3ba92fa95e9b6e1a2e1c55c2d7
Acer <Acer@DESKTOP-K09QF87>
2022-11-10 18:28:10 +0100

    Added src/types/tokentype.ts

---
+++ src/types/tokentype.ts
@@ -0,0 +1,11 @@
+
+/**
+ * This type is used when a new Token is created
+ */
+export type TokenType = {
+    tokenKey: string;
+    accountId: string;
+    creationDate: Date;
+    expireDate: Date;
+
+}
