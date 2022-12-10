41081430fa9c14c08a406164020cbd216514de65
Acer <Acer@DESKTOP-K09QF87>
2022-12-10 16:16:09 +0100

    Added src/routes/api/account/functions/search_api.ts

--- src/routes/api/account/account_api.ts
+++ src/routes/api/account/account_api.ts
@@ -11,6 +11,7 @@ import { Paths } from '../../../namespaces/paths';
 import { Schemas } from '../../../namespaces/schemas';
 import { loggedApi } from '../middlewares/middlewares_api';
 import { current_user_api } from './current_user_api';
+import { search_api } from './search_api';
 import { user_info_api } from './user_info_api';
 
 export const account_routes_api = express.Router();
@@ -19,22 +20,4 @@ account_routes_api.post('/current_user', loggedApi, current_user_api);
 
 account_routes_api.post('/user_info', loggedApi, user_info_api);
 
-account_routes_api.post('/profile/search', loggedApi, async(req,res)=>{
-    let query: string = req.body.query as string;
-    if(query && query != ""){
-        let mmis_data: MongoDbModelsManagerInterface = {
-            collection_name: process.env.MONGODB_ACCOUNTS_COLLECTION as string, schema: Schemas.ACCOUNTS
-        };
-        let accounts_data: AccountsInterface = {};
-        let accounts: Accounts = new Accounts(mmis_data,accounts_data);
-        await accounts.getAccounts({username: {$regex: `^${query}`, $options: "i"}}).then(result => {
-            let response: object = { done: true, msg: '', result: result['result'] };
-            return res.json(response);
-        }).catch(err => {
-            return res.status(500).json({done: false, msg: Messages.ERROR_SERVER});
-        });
-    }//if(query && query != ""){
-    else{
-        return res.status(400).json({done: false, msg: "Digita un termine di ricerca per continuare"});
-    }
-});
+account_routes_api.post('/profile/search', loggedApi, search_api);
---
+++ src/routes/api/account/search_api.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { Accounts, AccountsInterface } from "../../../classes/database/models/accounts";
+import { MongoDbModelsManagerInterface } from "../../../classes/database/mongodbmodelsmanager";
+import { Messages } from "../../../namespaces/messages";
+import { Schemas } from "../../../namespaces/schemas";
+
+export async function search_api(req: Request, res: Response){
+    let query: string = req.body.query as string;
+    if(query && query != ""){
+        let mmis_data: MongoDbModelsManagerInterface = {
+            collection_name: process.env.MONGODB_ACCOUNTS_COLLECTION as string, schema: Schemas.ACCOUNTS
+        };
+        let accounts_data: AccountsInterface = {};
+        let accounts: Accounts = new Accounts(mmis_data,accounts_data);
+        await accounts.getAccounts({username: {$regex: `^${query}`, $options: "i"}}).then(result => {
+            let response: object = { done: true, msg: '', result: result['result'] };
+            return res.json(response);
+        }).catch(err => {
+            return res.status(500).json({done: false, msg: Messages.ERROR_SERVER});
+        });
+    }//if(query && query != ""){
+    else{
+        return res.status(400).json({done: false, msg: "Digita un termine di ricerca per continuare"});
+    }
+}
