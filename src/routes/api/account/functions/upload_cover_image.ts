cc1658552ec28695454f2d045b09fd72010bd5d5
Acer <Acer@DESKTOP-K09QF87>
2022-12-12 22:55:08 +0100

    Added execution of set cover image folder operations in upload_cover_image function of upload_cover_image.ts

--- src/routes/api/account/functions/upload_cover_image.ts
+++ src/routes/api/account/functions/upload_cover_image.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import multiparty from "multiparty";
 import { parse } from "path";
+import { SetCoverImageFolder, SetCoverImageFolderInterface } from "../../../../classes/account/upload/setcoverimagefolder";
 import { Messages } from "../../../../namespaces/messages";
 import { Paths } from "../../../../namespaces/paths";
 
@@ -24,5 +25,17 @@ export async function upload_cover_image(req: Request, res: Response){
         tokenKey = fields["tokenKey"][0];
         //console.log(files);
         imagePath = files["image"][0]["path"];
+        const scifData: SetCoverImageFolderInterface = {
+            image_path: imagePath, token_key: tokenKey
+        };
+        const scif: SetCoverImageFolder = new SetCoverImageFolder(scifData);
+        scif.setFolder().then(result => {
+            if(result["done"] == true){
+                return res.status(200).json({ dest: result["dest"], done: true, msg: "Upload completato" });
+            }
+            else{
+                return res.status(500).json({done: false, msg: Messages.ERROR_COVER_IMAGE });
+            }
+        });
     });
 }
