52c25c127e9d839e959a3d6580b83d1c4818acf8
Acer <Acer@DESKTOP-K09QF87>
2022-12-13 09:39:08 +0100

    Added setBaseUrl function to search_api.ts

--- src/routes/api/account/functions/search_api.ts
+++ src/routes/api/account/functions/search_api.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { Accounts, AccountsInterface } from "../../../../classes/database/models/accounts";
 import { MongoDbModelsManagerInterface } from "../../../../classes/database/mongodbmodelsmanager";
 import { Messages } from "../../../../namespaces/messages";
+import { Paths } from "../../../../namespaces/paths";
 import { Schemas } from "../../../../namespaces/schemas";
 
 export async function search_api(req: Request, res: Response){
@@ -13,7 +14,11 @@ export async function search_api(req: Request, res: Response){
         let accounts_data: AccountsInterface = {};
         let accounts: Accounts = new Accounts(mmis_data,accounts_data);
         await accounts.getAccounts({username: {$regex: `^${query}`, $options: "i"}}).then(result => {
+            console.log("search_api getAccounts");
+            console.log(result);
             let response: object = { done: true, msg: '', result: result['result'] };
+            const baseUrl: string = `${req.protocol}://${req.get('host')}`;
+            response['_images']['profileImage'] = setBaseUrl(baseUrl,response['_images']['profileImage']);
             return res.status(200).json(response);
         }).catch(err => {
             return res.status(500).json({done: false, msg: Messages.ERROR_SERVER});
@@ -22,4 +27,16 @@ export async function search_api(req: Request, res: Response){
     else{
         return res.status(400).json({done: false, msg: "Digita un termine di ricerca per continuare"});
     }
+}
+
+/**
+ * Prepend the base url to a relative URL
+ * @param baseUrl the base url
+ * @param url the relative URL
+ * @returns the full URL that contains the base URL and the relative URL
+ */
+function setBaseUrl(baseUrl: string, url: string): string{
+    if(url && url != "")
+        return baseUrl+url;
+    return baseUrl+Paths.STATIC_IMG_DEFAULT+"/profile_image.jpg";
 }
