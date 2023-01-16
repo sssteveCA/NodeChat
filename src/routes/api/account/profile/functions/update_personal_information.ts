04d84f6f84903f678a84c1689079e68856db41a5
stepp <stepp@mail.com>
2023-01-16 13:51:42 +0100

    Added isset and not empty checks before update document to update_personal_information function in update_personal_information.ts

--- src/routes/api/account/profile/functions/update_personal_information.ts
+++ src/routes/api/account/profile/functions/update_personal_information.ts
@@ -3,21 +3,28 @@ import { SetPersonalInformation, SetPersonalInformationInterface } from "../../.
 import { Messages } from "../../../../../namespaces/messages";
 
 export function update_personal_information(req: Request, res: Response){
-    //let isset: boolean = (req.body["name"] && req.body["surname"] && req.body["sex"] &&   )
+    let isset: boolean = (req.body["name"] && req.body["surname"] && req.body["sex"] && req.body["birth_date"] && req.body["birth_place"] && req.body["living_place"]);
+    let notEmpty: boolean = (req.body["name"] != "" && req.body["surname"] != "" && req.body["sex"] != "" && req.body["birth_date"] != "" && req.body["birth_place"] != "" && req.body["living_place"] != "");
     try{
-        let spiData: SetPersonalInformationInterface = {
+        if(isset && notEmpty){
+            let spiData: SetPersonalInformationInterface = {
             token_key: res.locals.tokenKey,
             name: req.body["name"], surname: req.body["surname"], sex: req.body["sex"], 
             birth_date: req.body["birth_date"], birth_place: req.body["birth_place"], 
             living_place: req.body["living_place"]
+            }
+            let spi: SetPersonalInformation = new SetPersonalInformation(spiData);
+            spi.setPersonalInformation().then(result => {
+                if(result["done"] == true)
+                    return res.status(200).json({done: true, msg: "Informazioni personali aggiornate"});
+                else
+                    return res.status(500).json({done: false, msg: Messages.ERROR_PERSONALINFORMATION_UPDATE});
+            });
+        }//if(isset && notEmpty){
+        else{
+            return res.status(400).json({done: false, msg: Messages.ERROR_MISSINGDATA});
         }
-        let spi: SetPersonalInformation = new SetPersonalInformation(spiData);
-        spi.setPersonalInformation().then(result => {
-            if(result["done"] == true)
-                return res.status(200).json({done: true, msg: "Informazioni personali aggiornate"});
-            else
-                return res.status(500).json({done: false, msg: Messages.ERROR_PERSONALINFORMATION_UPDATE});
-        });
+        
         throw new Error;
     }catch(e){
         return res.status(500).json({done: false, msg: Messages.ERROR_PERSONALINFORMATION_UPDATE});
