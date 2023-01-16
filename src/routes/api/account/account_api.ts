8db15315b4c1735e3834647ceae1094bc4c1f656
stepp <stepp@mail.com>
2023-01-16 11:09:58 +0100

    Added '/profile/update_personal_information' route to account_api.ts

--- src/routes/api/account/account_api.ts
+++ src/routes/api/account/account_api.ts
@@ -16,6 +16,7 @@ import { user_info_api } from './functions/user_info_api';
 import multipart from "multiparty";
 import { upload_profile_image } from './functions/upload_profile_image';
 import { upload_cover_image } from './functions/upload_cover_image';
+import { update_personal_information } from './functions/update_personal_information';
 
 export const account_routes_api = express.Router();
 
@@ -26,5 +27,7 @@ account_routes_api.post('/user_info', loggedApi, user_info_api);
 account_routes_api.post('/profile/search', loggedApi, search_api);
 
 account_routes_api.post('/profile/upload_cover_image',upload_cover_image);
+
+account_routes_api.post('/profile/update_personal_information',update_personal_information);
 
 account_routes_api.post('/profile/upload_profile_image',upload_profile_image);
--- src/routes/api/account/functions/update_personal_information.ts
+++ src/routes/api/account/functions/update_personal_information.ts
@@ -1,5 +1,19 @@
-import { Request } from "express";
+import { Request, Response } from "express";
+import { SetPersonalInformation, SetPersonalInformationInterface } from "../../../../classes/account/update/setpersonalinformation";
+import { Messages } from "../../../../namespaces/messages";
 
-export function updatePersonalInformation(req: Request, res: Response){
-    
+export function update_personal_information(req: Request, res: Response){
+    let spiData: SetPersonalInformationInterface = {
+        token_key: res.locals.tokenKey,
+        name: req.body["name"], surname: req.body["surname"], sex: req.body["sex"], 
+        birth_date: req.body["birth_date"], birth_place: req.body["birth_place"], 
+        living_place: req.body["living_place"]
+    }
+    let spi: SetPersonalInformation = new SetPersonalInformation(spiData);
+    spi.setPersonalInformation().then(result => {
+        if(result["done"] == true)
+            return res.status(200).json({done: true, msg: "Informazioni personali aggiornate"});
+        else
+            return res.status(500).json({done: false, msg: Messages.ERROR_PERSONALINFORMATION_UPDATE});
+    });
 }
