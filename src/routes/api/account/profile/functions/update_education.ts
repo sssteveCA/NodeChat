2c475a39cee2dab4561088b4ff6a72b6a8668bd1
stepp <stepp@mail.com>
2023-01-17 17:25:45 +0100

    Added response to update_education function in update_education.ts

--- src/routes/api/account/profile/functions/update_education.ts
+++ src/routes/api/account/profile/functions/update_education.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { SetEducation, SetEducationInterface } from "../../../../../classes/account/update/seteducation";
 import { Messages } from "../../../../../namespaces/messages";
 
 export default function update_education(req: Request, res: Response){
@@ -6,7 +7,19 @@ export default function update_education(req: Request, res: Response){
     let notEmpty: boolean = (req.body["secondary_school"] != "" && req.body["university"] != "");
     try{
         if(isset && notEmpty){
-
+            let seData: SetEducationInterface = {
+                token_key: res.locals.tokenKey,
+                secondary_school: req.body["secondary_school"].trim(),
+                university: req.body["university"].trim()
+            }
+            let se: SetEducation = new SetEducation(seData);
+            se.setEducation().then(result => {
+                if(result["done"] == true)
+                    return res.status(200).json({done: true, msg: "Informazioni sull'istruzione aggiornate"});
+                else throw new Error;
+            }).catch(err => {
+                throw err;
+            });
         }//if(isset && notEmpty){
         else 
             return res.status(400).json({done: false, msg: Messages.ERROR_MISSINGDATA});
