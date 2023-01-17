6ced4aec06aa3dc00576840091fc9ce2748a3430
stepp <stepp@mail.com>
2023-01-17 15:22:09 +0100

    Added edUpdate and edUpdatePromise method to educationupdaterequest.ts

--- src/public/js/classes/requests/educationupdaterequest.ts
+++ src/public/js/classes/requests/educationupdaterequest.ts
@@ -37,5 +37,42 @@ export class EducationUpdateRequest{
         return this._error;
     }
 
+    public async edUpdate(): Promise<object>{
+        let response: object = {};
+        this._errno = 0;
+        try{
+            await this.edUpdatePromise().then(res => {
+                console.log(res);
+                response = JSON.parse(res);
+            }).catch(err => {
+                console.warn(err);
+                throw err;
+            })
+        }catch(e){
+            this._errno = EducationUpdateRequest.ERR_FETCH;
+            response = {done: false, msg: this.error}
+        }
+        return response;
+    }
+
+    private async edUpdatePromise(): Promise<string>{
+        let body: object = {
+            token_key: this._token_key, secondary_school: this._secondary_school, university: this._university
+        }
+        return await new Promise<string>((resolve,reject)=> {
+            fetch(EducationUpdateRequest.FETCH_URL,{
+                method: 'POST',
+                headers: {
+                    "Accept": "application/json", "Content-Type": "application/json"
+                },
+                body: JSON.stringify(body)
+            }).then(res => {
+                resolve(res.text());
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
+
 
 }
