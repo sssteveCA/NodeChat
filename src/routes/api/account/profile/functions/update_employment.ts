5bab6bbd0cef4030267eb39bc061a8c30ba5ee51
stepp <stepp@mail.com>
2023-01-19 23:16:09 +0100

    Added response to update_employment function in update_employment.ts

--- src/routes/api/account/profile/functions/update_employment.ts
+++ src/routes/api/account/profile/functions/update_employment.ts
@@ -1,15 +1,25 @@
 import { Request, Response } from "express";
+import { SetEmployment, SetEmploymentInterface } from "../../../../../classes/account/update/setemployment";
 import { Messages } from "../../../../../namespaces/messages";
 
 export default function update_employment(req: Request, res: Response){
     try{
         if(req.body["employment"] && req.body["employment"] != ""){
-
+            let empData: SetEmploymentInterface = {
+                token_key: res.locals.tokenKey, employment: req.body["exployment"].trim()
+            }
+            let emp: SetEmployment = new SetEmployment(empData);
+            emp.setEmployment().then(result => {
+                if(result["done"] == true)
+                    return res.status(200).json({done: true, msg: "Informazioni sul lavoro aggiornate"});
+                else
+                    return res.status(500).json({done: false, msg: Messages.ERROR_EMPLOYMENT_UPDATE});
+            })
         }//if(req.body["employment"] && req.body["employment"] != ""){
         else
             return res.status(400).json({done: false, msg: Messages.ERROR_MISSINGDATA});
     }catch(e){
-
+        return res.status(500).json({done: false, msg: Messages.ERROR_EMPLOYMENT_UPDATE});
     }
     
 }
