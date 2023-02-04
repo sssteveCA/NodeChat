a6106e5801a3a9d48cd6449d04146806e410c16b
stepp <stepp@mail.com>
2023-02-04 23:07:13 +0100

    Added condition to mustache views files to show cookie_banner partial

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
@@ -20,6 +21,8 @@ import { account_routes_api } from './routes/api/account/account_api';
 dotenv.config(); //Load .env file
 
 const app = express();
+
+app.use(cookieParser());
 app.use(session({
     secret: process.env.EXPRESS_SESSION_SECRET as string,
     saveUninitialized: false,
