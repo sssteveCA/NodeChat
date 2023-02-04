2f3eaa39b28e9713d89755ee7457c9ea34fbdf00
stepp <stepp@mail.com>
2023-02-04 20:02:27 +0100

    Updated menu.ts, setted position of '.policy-sub-menu' html item on hover

--- src/public/js/menu.ts
+++ src/public/js/menu.ts
@@ -30,20 +30,36 @@ function logout(): void{
 }
 
 function policy_menu_item(): void{
-    $('.policy-menu').on('mouseenter',(e)=>{
+    let policy_sub_menu: JQuery<HTMLUListElement> =  $('.policy-sub-menu');
+    let policy_menu: JQuery<HTMLUListElement> = $('.policy-menu');
+    let psm_css = {};
+    policy_menu.on('mouseenter',(e)=>{
         let pos = $(e.currentTarget).position();
-        let height: number = $(e.currentTarget).height() as number;
-        $('.policy-sub-menu').css({
+        let pm_height: number = $(e.currentTarget).height() as number;
+        let pm_width: number = policy_menu.width() as number;
+        let psm_width: number = policy_sub_menu.width() as number;
+        let half_width_diff = (psm_width - pm_width) / 2;
+        psm_css = {
             position: 'absolute',
-            top: (pos.top + height)+'px',
-            left: pos.left+'px',
+            top: (pos.top + pm_height)+'px',
+            left: (pos.left - half_width_diff)+'px',
             display: 'flex',
-            'z-index': 20
-        });
+            'z-index': 20,
+        }
+        policy_sub_menu.css(psm_css);
+
     });
-    $('.policy-menu').on('mouseleave',(e)=>{
-        $('.policy-sub-menu').css({
+    policy_menu.on('mouseleave',(e)=>{
+        policy_sub_menu.css({
             display: 'none'
         })
     });
+    policy_sub_menu.on('mouseenter',(e)=>{
+        $(e.currentTarget).css(psm_css)
+    });
+    policy_sub_menu.on('mouseleave',(e)=>{
+        $(e.currentTarget).css({
+            display: 'none'
+        })
+    })
 }
