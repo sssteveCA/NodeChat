0af5e5b34f75360f918e18c4dac61c2ccd8ceee9
stepp <stepp@mail.com>
2023-02-15 22:26:00 +0100

    Added setMessageHtml method to passwordconfirmdialog.ts

--- src/public/js/classes/dialogs/passwordconfirmdialog.ts
+++ src/public/js/classes/dialogs/passwordconfirmdialog.ts
@@ -6,12 +6,46 @@ export interface PasswordConfirmDialogInterface extends MessageDialogInterface{
 
 export class PasswordConfirmDialog extends MessageDialog{
 
+    private _password: JQuery<HTMLInputElement>;
+    private _conf_password: JQuery<HTMLInputElement>;
+
     constructor(data: PasswordConfirmDialogInterface){
         super(data);
     }
 
+    private setMessageHtml(): string{
+        return `
+<div class="container">
+    <div class="row">
+        <div class="col-12">${this._message}</div> 
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <label class="form-label">La tua password</label>
+            <input type="password" id="pcd-password" class="form-control">
+        </div>
+        <div class="col-12">
+            <input type="password" id="pcd-password" class="form-control">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <label class="form-label">Conferma password</label>
+        </div>
+        <div class="col-12">
+            <input type="password" id="pcd-password" class="form-control">
+        </div>
+    </div>
+    <div class="row">
+        <input type="checkbox" class="form-check-input" id="pcd-show-password">
+    </div>
+</div>
+            
+        `;
+    }
+
     protected setDialog(): void {
-        let message = this._message;
+        let message = this.setMessageHtml();
         this._dialog = $('<div>');
         this._dialog.attr('id',this._id);
         this.dialog.appendTo($('body'));
