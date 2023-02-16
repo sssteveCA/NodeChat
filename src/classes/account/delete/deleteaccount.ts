eb56c0de7e948c26f3317653c40fbe65a5ab124f
stepp <stepp@mail.com>
2023-02-16 18:55:37 +0100

    Added deleteAccount method to deleteaccount.ts

--- src/classes/account/delete/deleteaccount.ts
+++ src/classes/account/delete/deleteaccount.ts
@@ -1,4 +1,6 @@
 
+import { General } from '../../general';
+
 export interface DeleteAccountInterface{
     token_key: string;
     password: string;
@@ -7,10 +9,24 @@ export interface DeleteAccountInterface{
 export class DeleteAccount{
     private _token_key: string;
     private _password: string;
+    private _errno: number = 0;
+    private _error: string|null = null;
 
     constructor(data: DeleteAccountInterface){
         this._token_key = data.token_key;
         this._password = data.password;
+    }
+
+    get errno(){return this._errno;}
+    get error(){return this._error;}
+
+    public async deleteAccount(): Promise<object>{
+        let response: object = {};
+        this._errno = 0;
+        await General.getAccountId(this._token_key).then(accountId => {
+            
+        })
+        return response;
     }
 
     
