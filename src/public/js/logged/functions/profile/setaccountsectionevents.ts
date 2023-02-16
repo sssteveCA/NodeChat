0b450d77e60a4c3b5090b1e721d5d028a18e6956
stepp <stepp@mail.com>
2023-02-16 21:50:37 +0100

    Added message dialog to setAccountSectionEvents function in setaccountsectionevents.ts

--- src/public/js/logged/functions/profile/setaccountsectionevents.ts
+++ src/public/js/logged/functions/profile/setaccountsectionevents.ts
@@ -1,4 +1,7 @@
+import { MessageDialog, MessageDialogInterface } from "../../../classes/dialogs/messagedialog.js";
 import { AccountSettingsSection, AccountSettingsSectionInterface } from "../../../classes/template/profile/account_settings_section_events.js";
+import { fMessageDialog } from "../../../functions/general.js";
+import { Constants } from "../../../namespaces/constants.js";
 
 export function setAccountSectionEvents(): void{
     let asseData: AccountSettingsSectionInterface = {
@@ -6,6 +9,17 @@ export function setAccountSectionEvents(): void{
     }
     let asse: AccountSettingsSection = new AccountSettingsSection(asseData);
     asse.deleteAccountButtonClick((response)=>{
-
+        let mdData: MessageDialogInterface = {
+            title: 'Elimina account',
+            message: response[Constants.KEY_MESSAGE]
+        }
+        let md: MessageDialog = new MessageDialog(mdData);
+        md.btOk.on('click',()=> {
+            md.dialog.dialog('destroy');
+            md.dialog.remove();
+            if(response[Constants.KEY_DONE] == true){
+                
+            }
+        })
     });
 }
