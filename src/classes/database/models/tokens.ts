d9b8da961b1daa7ae482e8a343f32924915dd582
stepp <stepp@mail.com>
2023-02-17 22:13:52 +0100

    Added deleteTokens method to tokens.ts

--- src/classes/database/models/tokens.ts
+++ src/classes/database/models/tokens.ts
@@ -1,3 +1,4 @@
+import { DatabaseConnectionError } from "../../errors/databaseconnectionerror";
 import { MongoDbModelsManager, MongoDbModelsManagerInterface } from "../mongodbmodelsmanager";
 
 export interface TokensInterface{
@@ -20,6 +21,28 @@ export class Tokens extends MongoDbModelsManager{
                 break;
         }
         return this._error;
+    }
+
+    /**
+     * Delete all the token from the collection that match with a filter
+     * @param filter the filter to search the documents to delete
+     * @returns 
+     */
+    public async deleteTokens(filter: object): Promise<object>{
+        let response: object = {}
+        this._errno = 0;
+        await super.connect().then(conn => {
+            if(conn == true){
+                return super.delete(filter);
+            }
+            else throw new DatabaseConnectionError(this.error as string);
+        }).then(deleted => {
+            if(this._errno != 0) response = {done: true}
+            else throw new Error("")
+        }).catch(err => {
+            response = {done: false}
+        })
+        return response;
     }
 
 }
