f7a2149f90c2e8aa48ee80817856ebbbb87bf2a6
stepp <stepp@mail.com>
2023-03-08 23:36:06 +0100

    Added setFolder method to setphotofolder.ts

--- src/classes/account/upload/setphotofolder.ts
+++ src/classes/account/upload/setphotofolder.ts
@@ -5,7 +5,7 @@ import { MongoDbModelManagerInterface } from "../../database/mongodbmodelmanager
 import fs from "fs/promises";
 import { Constants } from "../../../namespaces/constants";
 import { Photo, PhotoInterface } from "../../database/models/photo";
-
+import { General } from "../../general";
 
 export interface SetPhotoFolderInterface{
     photo_path: string;
@@ -22,12 +22,12 @@ export class SetPhotoFolder{
     private static ERR_ACCOUNT_ID:number = 1;
     private static ERR_ACCOUNT_USERNAME:number = 2;
     private static ERR_MOVE_FILE:number = 3;
-    private static ERR_UPDATE_PROPERTY:number = 4;
+    private static ERR_ADD_PHOTO:number = 4;
 
     private static ERR_ACCOUNT_ID_MSG:string = "L'id dell'account non è stato trovato";
     private static ERR_ACCOUNT_USERNAME_MSG:string = "Lo username dell'account non è stato trovato";
     private static ERR_MOVE_FILE_MSG:string = "Impossibile spostare l'immagine";
-    private static ERR_UPDATE_PROPERTY_MSG:string = "Errore durante l'aggiornamento del database";
+    private static ERR_ADD_PHOTO_MSG:string = "Errore durante l'aggiornamento del database";
 
     constructor(data: SetPhotoFolderInterface){
         this._photo_path = data.photo_path;
@@ -48,14 +48,45 @@ export class SetPhotoFolder{
             case SetPhotoFolder.ERR_MOVE_FILE:
                 this._error = SetPhotoFolder.ERR_MOVE_FILE_MSG;
                 break;
-            case SetPhotoFolder.ERR_UPDATE_PROPERTY:
-                this._error = SetPhotoFolder.ERR_UPDATE_PROPERTY_MSG;
+            case SetPhotoFolder.ERR_ADD_PHOTO:
+                this._error = SetPhotoFolder.ERR_ADD_PHOTO_MSG;
                 break;
             default:
                 this._error = null;
                 break;
         }
         return this._error;
+    }
+
+    /**
+     * Set the path of the new uploaded image
+     */
+    public async setFolder(): Promise<object>{
+        this._errno = 0;
+        let response: object = {dest: null, done: false}
+        let accountId: string|null = await General.getAccountId(this._token_key);
+        if(accountId != null){
+            let accountUsername: string|null = await this.getAccountUsername(accountId);
+            if(accountUsername != null){
+                let moved: object = await this.moveFile(this._photo_path,accountUsername);
+                if(moved[Constants.KEY_DONE] == true){
+                    let addPhoto: object = await this.addPhotoDocument(accountId, moved["dest"],accountUsername);
+                    if(addPhoto[Constants.KEY_DONE] == true){
+                        response = {
+                            dest: addPhoto["absUrl"],
+                            done: true
+                        }
+                    }//if(addPhoto[Constants.KEY_DONE] == true){
+                    else 
+                        this._errno = SetPhotoFolder.ERR_ADD_PHOTO;
+                }//if(moved[Constants.KEY_DONE] == true){
+                else
+                    this._errno = SetPhotoFolder.ERR_MOVE_FILE;
+            }//if(accountUsername != null){
+        }//if(accountId != null){
+        else
+            this._errno = SetPhotoFolder.ERR_ACCOUNT_ID;
+        return response;
     }
 
     private async getAccountUsername(accountId: string): Promise<string|null>{
