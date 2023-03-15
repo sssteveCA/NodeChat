c769935bba81345ee3658561d079aee049d2bf39
stepp <stepp@mail.com>
2023-03-15 21:57:32 +0100

    Added getUserPhotos method to getphotos.ts

--- src/classes/account/get/getphotos.ts
+++ src/classes/account/get/getphotos.ts
@@ -1,3 +1,7 @@
+import { Constants } from "../../../namespaces/constants";
+import { Schemas } from "../../../namespaces/schemas";
+import { Photos } from "../../database/models/photos";
+import { MongoDbModelsManagerInterface } from "../../database/mongodbmodelsmanager";
 import { General } from "../../general";
 
 export interface GetPhotosInterface{
@@ -11,8 +15,10 @@ export class GetPhotos{
     private _error: string|null = null;
 
     private static ERR_ACCOUNT_ID:number = 1;
+    private static ERR_GET_PHOTOS:number = 2;
 
     private static ERR_ACCOUNT_ID_MSG:string = "L'id dell'account non è stato trovato";
+    private static ERR_GET_PHOTOS_MSG:string = "Errore durante la lettura delle immagini";
 
     constructor(data: GetPhotosInterface){
         this._token_key = data.token_key;
@@ -23,6 +29,9 @@ export class GetPhotos{
         switch(this._errno){
             case GetPhotos.ERR_ACCOUNT_ID:
                 this._error = GetPhotos.ERR_ACCOUNT_ID_MSG;
+                break;
+            case GetPhotos.ERR_GET_PHOTOS:
+                this._error = GetPhotos.ERR_GET_PHOTOS_MSG;
                 break;
             default:
                 this._error = null;
@@ -39,10 +48,25 @@ export class GetPhotos{
         this._errno = 0;
         let accountId: string|null = await General.getAccountId(this._token_key);
         if(accountId != null){
-
+            let photos_list: object = await this.getUserPhotos(accountId);
+            if(photos_list[Constants.KEY_DONE] == false)
+                this._errno = GetPhotos.ERR_GET_PHOTOS;
+            return photos_list;
         }//if(accountId != null){
         this._errno = GetPhotos.ERR_ACCOUNT_ID;
         return {done: false}
 
+    }
+
+    private async getUserPhotos(accountId: string): Promise<object>{
+        let mmis_data: MongoDbModelsManagerInterface = {
+            collection_name: process.env.MONGODB_PHOTOS_COLLECTION as string,
+            schema: Schemas.PHOTOS
+        }
+        let photos: Photos = new Photos(mmis_data,{});
+        await photos.getPhotos({accountId: accountId});
+        if(photos.errno == 0)
+            return { done: true,result: photos.results }
+        return { done: false }
     }
 }
