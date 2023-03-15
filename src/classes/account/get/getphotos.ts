ad336f58b223d61bf11fc28f6284981086d57831
stepp <stepp@mail.com>
2023-03-15 21:27:40 +0100

    Added getPhotos method to getphotos.ts

--- src/classes/account/get/getphotos.ts
+++ src/classes/account/get/getphotos.ts
@@ -1,3 +1,4 @@
+import { General } from "../../general";
 
 export interface GetPhotosInterface{
     token_key: string;
@@ -9,6 +10,10 @@ export class GetPhotos{
     private _errno: number = 0;
     private _error: string|null = null;
 
+    private static ERR_ACCOUNT_ID:number = 1;
+
+    private static ERR_ACCOUNT_ID_MSG:string = "L'id dell'account non è stato trovato";
+
     constructor(data: GetPhotosInterface){
         this._token_key = data.token_key;
     }
@@ -16,10 +21,28 @@ export class GetPhotos{
     get errno(){return this._errno;}
     get error(){
         switch(this._errno){
+            case GetPhotos.ERR_ACCOUNT_ID:
+                this._error = GetPhotos.ERR_ACCOUNT_ID_MSG;
+                break;
             default:
                 this._error = null;
                 break;
         }
         return this._error;
+    }
+
+    /**
+     * Get the photos uploaded by the logged user
+     * @returns 
+     */
+    public async getPhotos(): Promise<object>{
+        this._errno = 0;
+        let accountId: string|null = await General.getAccountId(this._token_key);
+        if(accountId != null){
+
+        }//if(accountId != null){
+        this._errno = GetPhotos.ERR_ACCOUNT_ID;
+        return {done: false}
+
     }
 }
