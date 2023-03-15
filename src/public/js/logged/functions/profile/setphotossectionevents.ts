741af36b1dffe1d5d2c62ca050cbadc522549006
stepp <stepp@mail.com>
2023-03-15 23:07:44 +0100

    Excluded from index 'dist/public/img/photos/*'

--- src/public/js/logged/functions/profile/setphotossectionevents.ts
+++ src/public/js/logged/functions/profile/setphotossectionevents.ts
@@ -11,13 +11,17 @@ export default function setPhotosSectionEvents(): void{
     let apse: AccountPhotosSectionEvents = new AccountPhotosSectionEvents(apseData);
     apse.getPhotos(response => {
         let pl_row: JQuery<HTMLDivElement> = $('#photos-list').find('.row') as JQuery<HTMLDivElement>;
+        pl_row.html("");
+        let row_content: string = "";
         response['result'].forEach(photo => {
             let img: string = `
 <div class="user-image col-12 col-sm-6 col-md-4 col-lg-3">
-    <img src="${photo['path']}" alt="" title="">
+    <img src="${photo['_path']}" alt="" title="">
 </div>           
             `;
+            row_content += img;
         })
+        pl_row.append(row_content);
     });
     apse.addPhotoButtonClick(response =>{
         let mdData: MessageDialogInterface = {
