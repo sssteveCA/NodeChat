c5a61633c9da44b8d1b6fa17d680a74a5e2e0213
stepp <stepp@mail.com>
2023-03-15 22:36:08 +0100

    Added gpRequest and gpRequestPromise methods to getphotosrequest.ts

--- src/public/js/classes/requests/getphotosrequest.ts
+++ src/public/js/classes/requests/getphotosrequest.ts
@@ -31,4 +31,38 @@ export class GetPhotosRequest{
         return this._error;
     }
 
+    public async gpRequest(): Promise<object>{
+        let response: object = {};
+        try{
+            await this.gpRequestPromise().then(res => {
+                console.log(res);
+                response = JSON.parse(res);
+            }).catch(err => {
+                console.warn(err);
+                throw err;
+            })
+        }catch(e){
+            response = {
+                done: false, message: "Errore durante il caricamento delle tue foto"
+            }
+        }
+        return response;
+    }
+
+    private async gpRequestPromise(): Promise<string>{
+        return await new Promise<string>((resolve,reject)=>{
+            fetch(GetPhotosRequest.FETCH_URL,{
+                method: 'POST',
+                headers: {
+                    "Accept": "application/json", "Content-Type": "application/json"
+                },
+                body: JSON.stringify({token_key: this._token_key})
+            }).then(res => {
+                resolve(res.text());
+            }).catch(err => {
+                reject(err);
+            })
+        });
+    }
+
 }
