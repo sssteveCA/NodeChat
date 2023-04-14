617a08136ec8aed10a6e953590e958bd1d316dd3
stepp <stepp@mail.com>
2023-04-14 22:43:25 +0200

    Added entry points to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,43 @@
 const {join, resolve} = require('path');
 
 const srcPath = join(__dirname,'src');
+const distPath = join(__dirname,'dist');
+const publicCssSrc = join(srcPath,'public','css');
+const publicJsSrc = join(srcPath,'public','js');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 
 module.exports = {
     entry: {
-
+        'css/aboutus': join(publicCssSrc,'aboutus.sass'),
+        'css/code_verify': join(publicCssSrc,'code_verify.sass'),
+        'css/contacts': join(publicCssSrc,'contacts.sass'),
+        'css/footer': join(publicCssSrc,'footer.sass'),
+        'css/index': join(publicCssSrc,'index.sass'),
+        'css/login': join(publicCssSrc,'login.sass'),
+        'css/menu': join(publicCssSrc,'menu.sass'),
+        'css/rules': join(publicCssSrc,'rules.sass'),
+        'css/subscribe': join(publicCssSrc,'subscribe.sass'),
+        'css/terms': join(publicCssSrc,'terms.sass'),
+        'css/verify': join(publicCssSrc,'verify.sass'),
+        'css/logged/index_logged': join(publicCssSrc,'logged/index_logged.sass'),
+        'css/logged/menu_logged': join(publicCssSrc,'logged/menu_logged.sass'),
+        'css/logged/profile': join(publicCssSrc,'logged/profile.sass'),
+        'css/logged/user_profile': join(publicCssSrc,'logged/user_profile.sass'),
+        'js/contacts': join(publicJsSrc,'contacts.ts'),
+        'js/footer': join(publicJsSrc,'footer.ts'),
+        'js/login': join(publicJsSrc,'login.ts'),
+        'js/menu': join(publicJsSrc,'menu.ts'),
+        'js/subscribe': join(publicJsSrc,'subscribe.ts'),
+        'js/verify': join(publicJsSrc,'verify.ts'),
+        'js/logged/index_logged': join(publicJsSrc,'logged/logged.ts'),
+        'js/logged/profile': join(publicJsSrc,'logged/profile.ts'),
+        'js/logged/user_profile': join(publicJsSrc,'logged/user_profile.ts'),
     },
     output: {
-        path: resolve(__dirname,'dist/public'),
-        filename: '{name}.js',
+        path: resolve(distPath,'public'),
+        filename: '[name].js',
         clean: true
     },
     module: {
