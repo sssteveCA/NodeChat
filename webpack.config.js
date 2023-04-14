9099c6f981727cb567342b183cdb8b48954e8f38
stepp <stepp@mail.com>
2023-04-15 00:53:49 +0200

    Added copy-webpack-plugin configuration to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,45 @@
 
-const {join, resolve} = require('path');
+const {join, resolve} = require('path')
 
-const srcPath = join(__dirname,'src');
-const distPath = join(__dirname,'dist');
-const publicCssSrc = join(srcPath,'public','css');
-const publicJsSrc = join(srcPath,'public','js');
-const publicImgPath = join(srcPath,'public','img');
+const srcPath = join(__dirname,'src')
+const distPath = join(__dirname,'dist')
+const srcCss = join(srcPath,'public','css')
+const srcJs = join(srcPath,'public','js')
+const srcImg = join(srcPath,'public','img')
+const srcFonts = join(srcPath,'public','fonts')
+const srcImgDist = join(distPath,'public','img')
+const srcFontsDist = join(distPath,'public','fonts');
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: {
-        'css/aboutus': join(publicCssSrc,'aboutus.sass'),
-        'css/code_verify': join(publicCssSrc,'code_verify.sass'),
-        'css/contacts': join(publicCssSrc,'contacts.sass'),
-        'css/footer': join(publicCssSrc,'footer.sass'),
-        'css/index': join(publicCssSrc,'index.sass'),
-        'css/login': join(publicCssSrc,'login.sass'),
-        'css/menu': join(publicCssSrc,'menu.sass'),
-        'css/rules': join(publicCssSrc,'rules.sass'),
-        'css/subscribe': join(publicCssSrc,'subscribe.sass'),
-        'css/terms': join(publicCssSrc,'terms.sass'),
-        'css/verify': join(publicCssSrc,'verify.sass'),
-        'css/logged/index_logged': join(publicCssSrc,'logged/index_logged.sass'),
-        'css/logged/menu_logged': join(publicCssSrc,'logged/menu_logged.sass'),
-        'css/logged/profile': join(publicCssSrc,'logged/profile.sass'),
-        'css/logged/user_profile': join(publicCssSrc,'logged/user_profile.sass'),
-        'js/contacts': join(publicJsSrc,'contacts.ts'),
-        'js/footer': join(publicJsSrc,'footer.ts'),
-        'js/login': join(publicJsSrc,'login.ts'),
-        'js/menu': join(publicJsSrc,'menu.ts'),
-        'js/subscribe': join(publicJsSrc,'subscribe.ts'),
-        'js/verify': join(publicJsSrc,'verify.ts'),
-        'js/logged/index_logged': join(publicJsSrc,'logged/index_logged.ts'),
-        'js/logged/profile': join(publicJsSrc,'logged/profile.ts'),
-        'js/logged/user_profile': join(publicJsSrc,'logged/user_profile.ts'),
+        'css/aboutus': join(srcCss,'aboutus.sass'),
+        'css/code_verify': join(srcCss,'code_verify.sass'),
+        'css/contacts': join(srcCss,'contacts.sass'),
+        'css/footer': join(srcCss,'footer.sass'),
+        'css/index': join(srcCss,'index.sass'),
+        'css/login': join(srcCss,'login.sass'),
+        'css/menu': join(srcCss,'menu.sass'),
+        'css/rules': join(srcCss,'rules.sass'),
+        'css/subscribe': join(srcCss,'subscribe.sass'),
+        'css/terms': join(srcCss,'terms.sass'),
+        'css/verify': join(srcCss,'verify.sass'),
+        'css/logged/index_logged': join(srcCss,'logged/index_logged.sass'),
+        'css/logged/menu_logged': join(srcCss,'logged/menu_logged.sass'),
+        'css/logged/profile': join(srcCss,'logged/profile.sass'),
+        'css/logged/user_profile': join(srcCss,'logged/user_profile.sass'),
+        'js/contacts': join(srcJs,'contacts.ts'),
+        'js/footer': join(srcJs,'footer.ts'),
+        'js/login': join(srcJs,'login.ts'),
+        'js/menu': join(srcJs,'menu.ts'),
+        'js/subscribe': join(srcJs,'subscribe.ts'),
+        'js/verify': join(srcJs,'verify.ts'),
+        'js/logged/index_logged': join(srcJs,'logged/index_logged.ts'),
+        'js/logged/profile': join(srcJs,'logged/profile.ts'),
+        'js/logged/user_profile': join(srcJs,'logged/user_profile.ts'),
     },
     output: {
         path: resolve(distPath,'public'),
@@ -51,7 +55,7 @@ module.exports = {
                     {
                         loader: 'ts-loader',
                         options: {
-                            configFile: resolve(publicJsSrc,'tsconfig.json')
+                            configFile: resolve(srcJs,'tsconfig.json')
                         }
                     }
                 ],
@@ -90,6 +94,12 @@ module.exports = {
         new FixStyleOnlyEntriesPlugin(),
         new MiniCssExtractPlugin({
             filename: '[name].css'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: srcImg, to: srcImgDist },
+                { from: srcFonts, to: srcFontsDist}
+            ]
         })
     ],
     resolve: {
