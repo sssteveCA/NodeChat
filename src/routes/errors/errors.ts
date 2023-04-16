261ac28f85bfe7f87bc433f2e73bb723592b19d3
stepp <stepp@mail.com>
2023-04-16 22:49:52 +0200

    Added src/routes/errors/functions/not_found.ts

--- src/routes/errors/errors.ts
+++ src/routes/errors/errors.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import session from 'express-session';
 import { Constants } from '../../namespaces/constants';
 import { Paths } from '../../namespaces/paths';
+import not_found from './functions/not_found';
 
 export const errors_router = express.Router();
 
@@ -12,14 +13,7 @@ errors_router.use(session({
     resave: false
 }));
 
-errors_router.get('/not_found',(req,res)=>{
-    let username: string = req.session[Constants.KEY_USERNAME] ? req.session[Constants.KEY_USERNAME] : null;
-    let guest: boolean = !username ? true : false;
-    return res.status(404).render('error/not_found',{
-        bootstrap_css: Paths.BOOTSTRAP_CSS, bootstrap_js: Paths.BOOTSTRAP_JS,
-        container: Constants.CONTAINER, guest: guest,jquery_js: Paths.JQUERY_JS, username: username
-    });
-});
+errors_router.get('/not_found',not_found);
 
 errors_router.get('/server_error',(req,res) => {
     let username: string = req.session[Constants.KEY_USERNAME] ? req.session[Constants.KEY_USERNAME] : null;
---
+++ src/routes/errors/functions/not_found.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import { Paths } from "../../../namespaces/paths";
+import path from 'path'
+import fs from 'fs/promises'
+import mustache from 'mustache'
+import { Constants } from "../../../namespaces/constants";
+
+export default async function not_found(req: Request, res: Response){
+    let not_found = path.resolve(Paths.ROOTPATH,'dist/views/partials/not_found.mustache')
+    let links_list: object[] = [
+        {rel: 'stylesheet', href: Paths.BOOTSTRAP_CSS},
+        {rel: 'stylesheet', href: '/css/menu.css'},
+        {rel: 'stylesheet', href: '/css/error/not_found.css'},
+        {rel: 'stylesheet', href: '/css/footer.css'},
+    ]
+    let scripts_list: object[] = [
+        {src: Paths.BOOTSTRAP_JS},
+        {src: Paths.JQUERY_JS},
+        {type: 'module', src: '/js/menu.js'},
+        {src: '/js/footer.js'},
+    ]
+    let partial_data: object = {}
+    if(req.session[Constants.KEY_USERNAME]){
+        partial_data = {token_key: req.session[Constants.KEY_TOKEN], username: req.session[Constants.KEY_USERNAME]}
+    }
+    let content = await fs.readFile(not_found, {encoding: 'utf-8'})
+    content = mustache.render(content,partial_data)
+    const data = {
+        token_key: req.session[Constants.KEY_TOKEN],
+        username: req.session[Constants.KEY_USERNAME],
+        title: "Pagina non trovata",
+        links: {
+            list: links_list,
+            link: function(){ return `<link rel="${(<any>this).rel}" href="${(<any>this).href}">`; }
+        },
+        scripts: {
+            list: scripts_list,
+            script: function(){
+                const type = ('type' in (<any>this)) ? (<any>this).type : "";
+                return `<script type="${type}" src="${(<any>this).src}"></script>`;
+            }
+        },
+        content : content
+    };
+    return res.render('layout',data)
+}
