72390fc7ef351986a36c5238764e3b7fb3511d47
stepp <stepp@mail.com>
2023-07-01 21:51:10 +0200

    Added getVideos method to account_videos_section_events.ts

--- src/public/js/classes/template/profile/account_videos_section_events.ts
+++ src/public/js/classes/template/profile/account_videos_section_events.ts
@@ -1,3 +1,4 @@
+import { GetVideosRequest, GetVideosRequestInterface } from "../../requests/getvideosrequest";
 
 export interface AccountVideosSectionEventsInterface{
     addVideoButton: JQuery<HTMLButtonElement>;
@@ -15,4 +16,16 @@ export class AccountVideosSectionEvents{
 
     get addVideoButton(){ return this._addVideoButton; }
     get addVideoInput(){ return this._addVideoInput; }
+
+    /**
+     * Execute the HTTP request to get the user videos
+     * @param callback the callback to invoke when a response is returned
+     */
+    public getVideos(callback: (response: object) => void): void{
+        let gvrData: GetVideosRequestInterface = {
+            token_key: $('input[name=token_key]').val() as string
+        }
+        let gvr: GetVideosRequest = new GetVideosRequest(gvrData)
+        gvr.gtRequest().then(res => callback(res))
+    }
 }
