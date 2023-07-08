8de283828c800c9b5a50d6c96b66e083a484da36
stepp <stepp@mail.com>
2023-07-08 11:58:40 +0200

    Added addVideoButtonClick method to account_videos_section_events.ts

--- src/public/js/classes/template/profile/account_videos_section_events.ts
+++ src/public/js/classes/template/profile/account_videos_section_events.ts
@@ -1,3 +1,4 @@
+import { AddVideoRequest, AddVideoRequestInterface } from "../../requests/addvideorequest";
 import { GetVideosRequest, GetVideosRequestInterface } from "../../requests/getvideosrequest";
 
 export interface AccountVideosSectionEventsInterface{
@@ -16,6 +17,28 @@ export class AccountVideosSectionEvents{
 
     get addVideoButton(){ return this._addVideoButton; }
     get addVideoInput(){ return this._addVideoInput; }
+
+    /**
+     * Execute the HTTP request to add a new video
+     * @param callback the callback to invoke when a response is returned
+     */
+    public addVideoButtonClick(callback: (response: object) => void): void{
+        this._addVideoButton.on('click',()=>{
+            this._addVideoInput.trigger('click');
+        });
+        this._addVideoInput.on('change',(e)=>{
+            if(e.target.files){
+                let avrData: AddVideoRequestInterface = {
+                    video: e.target.files[0] as File,
+                    token_key: $('input[name=token_key]').val() as string
+                }
+                let avr: AddVideoRequest = new AddVideoRequest(avrData);
+                avr.addVideo().then(res => {
+                    callback(res);
+                })
+            }
+        });
+    }
 
     /**
      * Execute the HTTP request to get the user videos
