52cedbd946edbc887c0e3fe21d2c8547711d4add
stepp <stepp@mail.com>
2023-07-08 13:42:56 +0200

    Added src/routes/api/account/profile/add/function/add_video.ts

---
+++ src/routes/api/account/profile/add/function/add_video.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import multiparty from "multiparty";
+import { Paths } from "../../../../../../namespaces/paths";
+import { Messages } from "../../../../../../namespaces/messages";
+import { General } from "../../../../../../classes/general";
+import { AddUserVideo, AddUserVideoInterface } from "../../../../../../classes/account/upload/adduservideo";
+import { Constants } from "../../../../../../namespaces/constants";
+
+export async function add_video(req: Request, res: Response){
+    let tokenKey: string = "";
+    let videoPath: string = "";
+    let fileName: string = "";
+    let form = new multiparty.Form({
+        autoFiles: true,
+        uploadDir: Paths.STATIC_UPLOAD
+    });
+
+    form.on('error',()=>{
+        return res.status(400).json({
+            done: false,
+            message: Messages.ERROR_ADD_VIDEO
+        })
+    })
+
+    form.parse(req, (error, fields, files) => {
+      tokenKey = General.getAuthToken(req);
+      videoPath = files["photo"][0]["path"];
+      fileName = files["photo"][0]["originalFilename"];
+      const auvData: AddUserVideoInterface = {
+        filename: fileName, video_path: videoPath, token_key: tokenKey
+      }
+      const auv: AddUserVideo = new AddUserVideo(auvData);
+      auv.addVideo().then(result => {
+        if(result[Constants.KEY_DONE] == true){
+            return res.status(201).json({ dest: result["dest"], done: true, message: "Video caricato correttamente"});
+        }
+        else{
+            return res.status(500).json({
+                done: false, message: Messages.ERROR_ADD_VIDEO
+            });
+        }
+      })
+
+    })
+}
--- src/routes/api/account/profile/add/profile_add.ts
+++ src/routes/api/account/profile/add/profile_add.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { add_photo } from "./function/add_photo";
 import { upload_cover_image } from "./function/upload_cover_image";
 import { upload_profile_image } from "./function/upload_profile_image";
+import { add_video } from "./function/add_video";
 
 export const profile_add_routes = express.Router();
 
@@ -10,3 +11,5 @@ profile_add_routes.post('/photo',add_photo);
 profile_add_routes.post('/cover_image',upload_cover_image);
 
 profile_add_routes.post('/profile_image',upload_profile_image);
+
+profile_add_routes.post('/video',add_video);
